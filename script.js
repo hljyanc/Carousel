a1afeceb24cd0b2df94640d5012bee432bbb55f7
hljyanc <hljyanc@gmail.com>
2021-06-25 17:01:01 -0700

    Make slide auto run

--- script.js
+++ script.js
@@ -1,34 +1,51 @@
 let slideIndex = 1;
-displaySlide(slideIndex);
+const totalSlide = 3;
+const slideName = "slide";
 
-function displaySlide (n) {
-    let slides=document.getElementsByClassName("display-img");
-    let dots = document.getElementsByClassName("dot");
+let dots = document.getElementsByClassName("dot");
 
-    //Reassign slideIndex value
-    if(n>slides.length){
-        slideIndex = 1;
-    } else if(n == 0){
-        slideIndex =slides.length;
-    } else{
-        slideIndex = n;
-    } 
+autoRun();
 
-
-    for( i = 0; i<slides.length; i++){
-        if(i==slideIndex-1){
-            slides[i].style.display="block";
+ //Change image src to display and update the dot style
+function currentSlide(){
+    document.getElementById("display-img").src = "images/"+ slideName + slideIndex + ".jpg";
+    for( let i = 0; i<totalSlide; i++){
+        if(i === slideIndex-1){
             dots[i].classList.add("active");
   
         }else{
-            slides[i].style.display="none";
             dots[i].classList.remove("active");
         }
     }
 }
 
 
+
+function displaySlide (n) {
+
+    //Reassign slideIndex value
+    if( n > totalSlide){
+        slideIndex = 1;
+    } else if(n === 0){
+        slideIndex =totalSlide;
+    } else{
+        slideIndex = n;
+    } 
+
+    currentSlide();
+}
+
+
 function changeSlide (n) {
-    displaySlide(slideIndex +=n);
-    console.log(slideIndex);
+    displaySlide(slideIndex +=n);   
+}
+
+
+function autoRun(){
+    slideIndex++;
+    if(slideIndex>totalSlide){
+        slideIndex = 1;
+    }
+    currentSlide();
+    setTimeout(autoRun, 2000);
 }
